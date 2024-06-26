743d8d385ce1426b550d3d1f2297a60e556f49b9
chandan <chandan@castler.com>
2024-06-27 00:08:21 +0530

    Third Commit

--- src/main/java/com/castler/jenkins/CastlerJenkinsServiceApplication.java
+++ src/main/java/com/castler/jenkins/CastlerJenkinsServiceApplication.java
@@ -1,16 +1,16 @@
-//package com.castler.jenkins;
-//
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//
-//@SpringBootApplication
-//public class CastlerJenkinsServiceApplication {
-//
-//	public static void main(String[] args) {
-//
-//
-//		SpringApplication.run(CastlerJenkinsServiceApplication.class, args);
-//		System.out.println("This is my first maven project with Jenkins !!");
-//	}
-//
-//}
+package com.castler.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CastlerJenkinsServiceApplication {
+
+	public static void main(String[] args) {
+
+
+		SpringApplication.run(CastlerJenkinsServiceApplication.class, args);
+		//System.out.println("This is my first maven project with Jenkins !!");
+	}
+
+}
